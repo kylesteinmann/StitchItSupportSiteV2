1b9948974012dc53add2ba4a88d905245caa9044
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2022-10-19 14:20:18 -0500

    Created Componets and edits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { SupportFormComponent } from './Components/support-form/support-form.component';
+import { SubmitSuccessComponent } from './Components/submit-success/submit-success.component';
+import { TrainingComponent } from './Components/training/training.component';
+import { KnowledgebaseComponent } from './Components/knowledgebase/knowledgebase.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SupportFormComponent,
+    SubmitSuccessComponent,
+    TrainingComponent,
+    KnowledgebaseComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
